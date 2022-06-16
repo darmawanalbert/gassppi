6dc6fbf3ef477b21ec34c8bf2a79b1865851ef53
Albert Darmawan <darmawanalbert@gmail.com>
2022-06-16 21:43:03 +1000

    chore: GitHub Action Workflow for Sanity Test (#2)

--- constant/directory_constant.py
+++ constant/directory_constant.py
@@ -1,5 +1,6 @@
+import os
 # List of directory paths
-repository_path = "/Users/albertdarmawan/Documents/gassppi/"
-dbd5_path = repository_path + "data/dbd5/"
-masif_path = repository_path + "data/masif-test/"
-templates_path = repository_path + "data/templates/"
+repository_path = os.getcwd()
+dbd5_path = repository_path + "/data/dbd5/"
+masif_path = repository_path + "/data/masif-test/"
+templates_path = repository_path + "/data/templates/"
--- run.py
+++ run.py
@@ -5,8 +5,8 @@ from utility.load_ppi_templates import load_ppi_templates
 from utility.execute_with_elapsed_time import execute_with_elapsed_time
 from constant.directory_constant import dbd5_path, templates_path
 from constant.last_heavy_atom import lha_dict
-from constant.dbd5_dataset import dbd5_ox_list
-from benchmark.dbd_all_templates import dbd_all_templates
+from constant.dbd3_dataset import dbd3_ab_list
+from benchmark.dbd_sanity_test import dbd_sanity_test
 
 pdb_parser = PDBParser()
 
@@ -24,7 +24,6 @@ dbd5_or_templates_5a_dict = load_ppi_templates(templates_path, "dbd5_or_template
 dbd5_all_templates_5a_dict = {**dbd5_a_templates_5a_dict, **dbd5_ei_templates_5a_dict, **dbd5_er_templates_5a_dict,
                       **dbd5_es_templates_5a_dict, **dbd5_ab_templates_5a_dict, **dbd5_og_templates_5a_dict,
                       **dbd5_ox_templates_5a_dict, **dbd5_or_templates_5a_dict}
-print(len(dbd5_all_templates_5a_dict))
 
 # Docking Benchmark 3: Load PPI Templates (5A)
 # Load DBD3 PPI template dictionary from .json files (to be used in the main program)
@@ -35,13 +34,12 @@ dbd3_o_templates_5a_dict = load_ppi_templates(templates_path, "dbd3_o_templates_
 
 dbd3_all_templates_5a_dict = {**dbd3_e_templates_5a_dict, **dbd3_a_templates_5a_dict, **dbd3_ab_templates_5a_dict,
                       **dbd3_o_templates_5a_dict}
-print(len(dbd3_all_templates_5a_dict))
 
 
-print("DBD5: Others, miscellaneous (OX)")
-execute_with_elapsed_time(dbd_all_templates,
-                        pdb_id_list=dbd5_ox_list,
-                        templates_dict=dbd5_all_templates_5a_dict,
+print("DBD3: AB")
+execute_with_elapsed_time(dbd_sanity_test,
+                        pdb_id_list=dbd3_ab_list,
+                        templates_dict=dbd3_ab_templates_5a_dict,
                         dbd_path=dbd5_path,
                         pdb_parser=pdb_parser,
                         lha_dict=lha_dict,
