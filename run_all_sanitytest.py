b949ab3d6f9b07356a5487773322e0eb76c329a1
Albert Darmawan <darmawanalbert@gmail.com>
2022-06-20 13:34:08 +1000

    Add MaSIF transient dataset, Run all DBD3 sanity tests on GitHub actions

--- run_all_sanitytest.py
+++ run_all_sanitytest.py
@@ -1,11 +1,11 @@
 from Bio.PDB import *
 
-from utility.load_pdb import load_pdb
 from utility.load_ppi_templates import load_ppi_templates
 from utility.execute_with_elapsed_time import execute_with_elapsed_time
 from constant.directory_constant import dbd5_path, templates_path
 from constant.last_heavy_atom import lha_dict
-from constant.dbd3_dataset import dbd3_ab_list
+from constant.dbd3_dataset import dbd3_all_list
+from constant.dbd5_dataset import dbd5_all_list
 from benchmark.dbd_sanity_test import dbd_sanity_test
 
 pdb_parser = PDBParser()
@@ -36,10 +36,27 @@ dbd3_all_templates_5a_dict = {**dbd3_e_templates_5a_dict, **dbd3_a_templates_5a_
                       **dbd3_o_templates_5a_dict}
 
 
-print("DBD3: AB")
+print("DBD3: All Complexes")
 execute_with_elapsed_time(dbd_sanity_test,
-                        pdb_id_list=dbd3_ab_list,
-                        templates_dict=dbd3_ab_templates_5a_dict,
+                        pdb_id_list=dbd3_all_list,
+                        templates_dict=dbd3_all_templates_5a_dict,
+                        dbd_path=dbd5_path,
+                        pdb_parser=pdb_parser,
+                        lha_dict=lha_dict,
+                        ranking_size=100,
+                        verbose=True,
+                        iteration_per_protein=1,
+                        population_size=400,
+                        number_of_generations=200,
+                        crossover_probability=0.9,
+                        mutation_probability=0.9,
+                        tournament_size=2,
+                        number_of_tournament=20)
+
+print("DBD5: All Complexes")
+execute_with_elapsed_time(dbd_sanity_test,
+                        pdb_id_list=dbd5_all_list,
+                        templates_dict=dbd5_all_templates_5a_dict,
                         dbd_path=dbd5_path,
                         pdb_parser=pdb_parser,
                         lha_dict=lha_dict,
---
+++ run_dbd3_a_sanitytest.py
@@ -0,0 +1,30 @@
+from Bio.PDB import *
+
+from utility.load_ppi_templates import load_ppi_templates
+from utility.execute_with_elapsed_time import execute_with_elapsed_time
+from constant.directory_constant import dbd5_path, templates_path
+from constant.last_heavy_atom import lha_dict
+from constant.dbd3_dataset import dbd3_a_list
+from benchmark.dbd_sanity_test import dbd_sanity_test
+
+pdb_parser = PDBParser()
+
+# Load DBD3 PPI template dictionary from .json files (to be used in the main program)
+dbd3_a_templates_5a_dict = load_ppi_templates(templates_path, "dbd3_a_templates_5a.json")
+
+print("DBD3: A")
+execute_with_elapsed_time(dbd_sanity_test,
+                        pdb_id_list=dbd3_a_list,
+                        templates_dict=dbd3_a_templates_5a_dict,
+                        dbd_path=dbd5_path,
+                        pdb_parser=pdb_parser,
+                        lha_dict=lha_dict,
+                        ranking_size=100,
+                        verbose=True,
+                        iteration_per_protein=1,
+                        population_size=400,
+                        number_of_generations=200,
+                        crossover_probability=0.9,
+                        mutation_probability=0.9,
+                        tournament_size=2,
+                        number_of_tournament=20)
